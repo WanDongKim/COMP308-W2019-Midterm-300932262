e7d02132f483e3906afe3060aff91fb4cea69514
Dongwan Kim <zeumes12@gmail.com>
2019-02-23 14:27:52 -0500

    Added the edit page and functionality

--- server/routes/books.js
+++ server/routes/books.js
@@ -85,7 +85,24 @@ router.post('/:id', (req, res, next) => {
     /*****************
      * ADD CODE HERE *
      *****************/
+    let id = req.params.id;
+    let updatedBook = book({
+      "_id": id,
+      "Title": req.body.title,
+      "Price": req.body.price,
+      "Author": req.body.author,
+      "Genre": req.body.genre
+    });
 
+    book.update({_id:id}, updatedBook, (err) =>{
+      if(err){
+        console.log(err);
+        res.end(err);
+      }else{
+        //refresh the book list
+        res.redirect('/books');
+      }
+    })
 });
 
 // GET - process the delete by user id
